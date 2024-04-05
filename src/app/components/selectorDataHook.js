0a1a8fc7d89d277a177ad5115685c3911d20430b
beausalgado <beausalgado@gmail.com>
2024-04-05 19:41:28 -0300

    category selector files

---
+++ src/app/components/selectorDataHook.js
@@ -0,0 +1,7 @@
+// import { useEffect} from 'react';
+
+// export default async function getSelectorData() {
+//     const res = await fetch(process.env.SELECTOR_API);
+//     const categories = await res.json()
+//     return categories;
+//   }
