4b873a3211be9988d94820a42f1e3678a102df42
beausalgado <beausalgado@gmail.com>
2024-04-05 20:17:04 -0300

    a server side api call for my client component

---
+++ src/app/components/apicall.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+const APIData = process.env.SELECTOR_API
+//console.log(APIData)
+
+// async function getSelectorData() {
+//   console.log("selector data")
+//   const res = await fetch();
+//   console.log("fetching selector data")
+//   const categories = await res.json();
+//   console.log("fetching selector data json")
+//   console.log(categories)
+//   return categories;
+// }
+
+ export const [selectorData, setSelectorData] = useState([]);
+
+  useEffect(() => {
+    console.log("fetch")
+    //console.log(APIData)
+    fetch("https://api.mercadolibre.com/sites/MLA/categories")
+      .then((res) => res.json())
+      .then((data) => {
+        setSelectorData(data)
+      })
+    console.log("fetch1")
+  }, []);
